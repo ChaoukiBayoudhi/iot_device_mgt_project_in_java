2ff18983689e880b83f234d5b96cbff6a83f7af5
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2025-11-03 10:10:52 +0100

    First Commit

--- src/tn/sesame/iot/IoTDevice.java
+++ src/tn/sesame/iot/IoTDevice.java
@@ -1,43 +1,17 @@
 package tn.sesame.iot;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of="id")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Accessors(fluent = true)
 public class IoTDevice {
-    private long id;
-    private String type;
-    private boolean connected;
-
-    public IoTDevice(long id1, String type1) {
-        id = id1;
-        type = type1;
-        connected=false;
-    }
- //redefinition de constructeur
-    public IoTDevice(long id, String type, boolean connected) {
-        this.id = id;
-        this.type = type;
-        this.connected = connected;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public boolean isConnected() {
-        return connected;
-    }
-
-    public void setConnected(boolean connected) {
-        this.connected = connected;
-    }
+     long id;
+     String type;
+     boolean connected;
 }
