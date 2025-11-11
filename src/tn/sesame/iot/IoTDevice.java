096518ea759304b1cb344d295dd1b9d14b07dc38
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2025-11-11 11:33:18 +0100

    Update the IoTDevice class

--- src/tn/sesame/iot/IoTDevice.java
+++ src/tn/sesame/iot/IoTDevice.java
@@ -3,94 +3,75 @@ package tn.sesame.iot;
 import lombok.*;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
+
 //@Builder
 //@AllArgsConstructor
 @Getter
 @Setter
 //@NoArgsConstructor
 @ToString
-@EqualsAndHashCode(of="id")
-@RequiredArgsConstructor
+@EqualsAndHashCode(of = "id")
+//@RequiredArgsConstructor
 //@Data // generates getters, setters, no args constructor,
 // required args constructor,redefine toString, equals and hashCode methods
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Accessors(fluent = true,chain=true)
+@Accessors(fluent = true, chain = true)
 public class IoTDevice {
-     long id;
-     static long lastId=0;
-     @NonNull
-     String type;
-     boolean connected;
-     @NonNull
-     String manufacturer;
-     @NonNull
-     String firmwareVersion;
-     @NonNull
-     String model;
-     int batteryLevel;
-     long lastSeen;
-     @NonNull
-     String location;
-     boolean active;
-     static final int MAX_DEVICE_ID_LENGTH=20;
-     static final int MIN_DEVICE_ID_LENGTH=3;
-     static int deviceCount;
+    @NonNull
+    final String id;
+    @NonNull
+    String type;
+    boolean connected;
+    @NonNull
+    String manufacturer;
+    @NonNull
+    String firmwareVersion;
+    @NonNull
+    String model;
+    @NonNull
+    String location;
+    boolean active;
+    int batteryLevel;
+    long lastSeen;
+    static final long MAX_DEVICE_ID_LENGTH = 20;
+    static final long MIN_DEVICE_ID_LENGTH = 3;
+    static int countDevice;
 
-    public IoTDevice() {
-        id=++lastId;
-    }
-
-    public IoTDevice(String type, boolean connected, String manufacturer, String firmwareVersion, String model, int batteryLevel, long lastSeen, String location, boolean active) {
-        id=++lastId;
-        //this();//call the no ags constructor
+    public IoTDevice(@NonNull String id, @NonNull String type, boolean connected, @NonNull String manufacturer, @NonNull String firmwareVersion, @NonNull String model, @NonNull String location, boolean active, int batteryLevel, long lastSeen) {
+//        if (id.trim().length() < MIN_DEVICE_ID_LENGTH||id.trim().length()>MAX_DEVICE_ID_LENGTH)
+//            if(!(id.trim().matches("^[a-zA-Z0-9]+$")))
+        //or
+        if(!(id.trim().matches("^[a-zA-Z0-9]{3,20}$")))
+                throw new IllegalArgumentException("Error verify the length and the format of the id.");
+        this.id = id;
         this.type = type;
         this.connected = connected;
         this.manufacturer = manufacturer;
+        if(firmwareVersion.trim().matches("^[0-9]+\\.[0-9]+\\.[0-9]+$"))
+            throw new IllegalArgumentException("Error ! Verify the format of the version (X.Y.Z)");
         this.firmwareVersion = firmwareVersion;
         this.model = model;
-        this.batteryLevel = batteryLevel;
-        this.lastSeen = lastSeen;
         this.location = location;
         this.active = active;
+        if(batteryLevel<0 || batteryLevel>100)
+            throw new IllegalArgumentException("Error ! Verify the percent value.");
+        this.batteryLevel = batteryLevel;
+        this.lastSeen = lastSeen;
+    }
+    //required args constructor
+    public IoTDevice(@NonNull String id, @NonNull String type,  @NonNull String manufacturer, @NonNull String firmwareVersion, @NonNull String model, @NonNull String location) {
+        if(!(id.trim().matches("^[a-zA-Z0-9]{3,20}$")))
+            throw new IllegalArgumentException("Error verify the length and the format of the id.");
+        this.id = id;
+        this.type = type;
+        this.manufacturer = manufacturer;
+        if(firmwareVersion.trim().matches("^[0-9]+\\.[0-9]+\\.[0-9]+$"))
+            throw new IllegalArgumentException("Error ! Verify the format of the version (X.Y.Z)");
+        this.firmwareVersion = firmwareVersion;
+        this.model = model;
+        this.location = location;
     }
 
     //methods
-    public boolean connect(int timeout) throws Exception{
-        long startTime=System.currentTimeMillis();
-        IO.println("connecting a new device (firmwareVersion = "+ firmwareVersion+", model = "+model);
-        Thread.sleep(500);
-        connected=true;
-        long endTime=System.currentTimeMillis();
-        long time=endTime-startTime;
-        if(time<timeout)
-        {
-            IO.println("Connection successfully establish");
-            return true;
-        }
-        return false;
-    }
-    public boolean connect(String protocol, int timeout) throws Exception
-    {
-        long startTime=System.currentTimeMillis();
-        IO.println("connecting a new device (firmwareVersion = "+ firmwareVersion+", model = "+model+")");
-        Thread.sleep(500);
-        connected=true;
-        long endTime=System.currentTimeMillis();
-        long time=endTime-startTime;
-        if(time<timeout)
-        {
-            IO.println("Connection successfully establish using the protocol" + protocol);
-            return true;
-        }
-        return false;
-    }
-
-    public void disconnect()throws Exception
-    {
-        IO.println("disconnecting device ...");
-        Thread.sleep(100);
-        connected=false;
-        IO.println("device (firmwareVersion = "+ firmwareVersion+ ", model = "+model+")");
-    }
 
 }
